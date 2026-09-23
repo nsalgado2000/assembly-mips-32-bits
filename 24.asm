72d19d28ebce6cddd8b5596b2281ce1eb513910d
Nicolas Salgado <2000nsalgado@gmail.com>
2026-09-23 11:01:39 -0300

    24

---
+++ 24.asm
@@ -0,0 +1,4 @@
+escreva uma sequência de instruções em MIPS que armazene no registrador **$t1** a seguinte constante de 32 bits [QuestoesAnterioresProvas.pdf]: `0000 0000 0000 0001 0000 0000 0000 0001`
+
+lui  $t1, 0x0000
+ori $t1, $t1, 0x0001
