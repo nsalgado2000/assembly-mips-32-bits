5920a1789431ff848eba2658b5a56197d36e5088
Nicolas Salgado <2000nsalgado@gmail.com>
2026-09-22 22:36:50 -0300

    16

---
+++ 16.asm
@@ -0,0 +1,14 @@
+Considerando que $s4 é a variável f e $s2 é a variável h:
+
+
+sll $s2, $s4, 1
+
+
+
+
+Qual é a operação equivalente em linguagem C que essa linha está fazendo?
+
+#0010 -> 0100
+#está indo de 2 para 4, então ta multiplicando por 2, logo: 
+
+h = f*2
