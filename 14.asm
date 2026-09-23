aaee9eb86ca56c9d6277b09ba7792b954bdeefc7
Nicolas Salgado <2000nsalgado@gmail.com>
2026-09-22 22:29:36 -0300

    14

---
+++ 14.asm
@@ -0,0 +1,16 @@
+
+while (i > j) {
+    f = f + g;
+}
+
+
+
+
+# f = $s0, g = $s1, i = $s2, j = $s3
+
+loop:   
+      slt $t1, $s3, $s2
+      beq $t1, $zero, fim
+      add $s0, $s0, $s1
+      j loop
+fim:  
